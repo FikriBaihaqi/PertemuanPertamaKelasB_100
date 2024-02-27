a6c9ddd5ff8e6fa4339b32283a88819f06a8df97
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-02-27 11:46:38 +0700

    membuat pseudocode untuk mencari luas persegi panjang

--- PraktikumPertemuan1/PraktikumPertemuan1.cpp
+++ PraktikumPertemuan1/PraktikumPertemuan1.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main()
-{
-
-}
+{  // begin
+      // numeric nPanjang, nLebar, nLuas
+      // display 'Masukkan Panjang'
+      // accept nPanjang
+      // diplay 'Masukkan Lebar'
+      // accept nLebar
+      // compute nLuas = nPanjang * nLuas
+      // display 'Luasnya adalah = ' + nLuas
+}  // end
